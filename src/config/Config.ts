04d5a33f05ee98ff834d01cfb3e8420b2e862c9f
Aleksandr Mihasenko <aleksandr.mihasenko@gmail.com>
2022-09-18 14:37:01 +0300

    deploy

--- src/config/Config.ts
+++
@@ -1,9 +0,0 @@
-export default class Config {
-  
-  constructor(
-    public step: number = 0,
-    public maxStep: number = 6,
-    public sizeCell: number = 16,
-    public sizeFruit: number = sizeCell / 4,
-  ) {}
-}
--- src/index.ts
+++
@@ -1,6 +0,0 @@
-import './styles/styles.sass';
-import Game from './views/Game';
-
-const container = document.querySelector('#board-canvas');
-
-new Game(container);
